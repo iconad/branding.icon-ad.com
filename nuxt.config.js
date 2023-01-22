9e3799313d23c91526e9e723bd68a175d604b003
iconad <amir@icon-ad.com>
2023-01-22 17:12:03 +0400

    imac to m1

---
+++ nuxt.config.js
@@ -0,0 +1,126 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: "Creative Branding Studio in Dubai, UAE | ICON BrandingStudio",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "google-site-verification", content: "lm7aAz0cUFowoNG5XRfmf59aaD2PWne5Uvup3RIME_Y" },
+      { name: "p:domain_verify", content: "edb35027c7b02f730c5fbea295b14924" },
+      { hid: 'og:image', property: 'og:image', content: "/icon.png" },
+      {
+        hid: "title",
+        name: "title",
+        content: "Creative Branding Studio in Dubai, UAE | ICON BrandingStudio"
+      },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Icon Branding Studio is an award winning branding & design Studio based in Dubai helping businesses grow through distinctive branding & inspiring design. Contact us now"
+      },
+      {
+        hid: "keywords",
+        name: "keywords",
+        content:
+          "branding, marketing strategy, graphic design, brochure design, branding agencies in dubai, advertising companies in dubai, branding agency dubai, branding agency dubai, digital marketing companies dubai, event management companies in dubai, event companies in dubai, digital marketing, web design, seo company dubai, web development companies in dubai, web design company dubai, digital agency dubai, creative agency dubai, motion graphics, 3d rendering, branding strategy"
+      }
+    ],
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    "~/assets/css/fonts.css",
+    "~/assets/css/style.css"
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    { src: "~/plugins/vue-tel-input"},
+    { src: "~/plugins/vimeo-player", mode: "client" },
+    { src: "~/plugins/vue-carousel", mode: "client" },
+    { src: "~/plugins/vue-siema", mode: "client" },
+    { src: "~/plugins/vue-scroll", mode: "client" },
+    { src: "~/plugins/vue-gsap", mode: "client" },
+    { src: "~/plugins/vue-progressive-image", mode: "client" },
+    { src: "~/plugins/vue-easy-lightbox", mode: "client" },
+    { src: "~/plugins/vue-masonry", mode: "client" }
+  ],
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
+
+  router: {
+    middleware: ["visit"],
+    // linkActiveClass: "active-link"
+  },
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: ['@nuxtjs/axios'],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+
+  axios: {
+    baseURL: 'https://drupal.icon-ad.com/api', // Used as fallback if no runtime config is provided
+  },
+
+  server: {
+    port: 3002 // default: 3000
+  },
+
+  loading: {
+    color: '#F4505F',
+    height: '5px'
+  },
+
+
+  cache: {
+    useHostPrefix: false,
+    pages: [
+      '/',
+    ],
+    store: {
+      type: 'memory',
+      max: 100,
+      ttl: 60,
+    },
+  },
+
+  sitemap: {
+    path: '/sitemap.xml',
+    hostname: 'https://branding.icon-ad.com',
+    cacheTime: 1000 * 60 * 15,
+    gzip: true,
+    generate: false,
+    routes: [
+      '/',
+      '/projects',
+      '/expertise'
+    ].map(route => ({
+      url: route,
+      changefreq: 'monthly',
+      priority: 1,
+      lastmodISO: new Date().toISOString().split('T')[0]
+    }))
+  },
+
+  gtm: {
+    id: 'GTM-MV67C85', // Used as fallback if no runtime config is provided
+  },
+
+  publicRuntimeConfig: {
+    gtm: {
+      id: process.env.GOOGLE_TAG_MANAGER_ID
+    }
+  },
+
+
+
+}
